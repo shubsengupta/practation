f7de2d18ea1037d45ec6fb8e173aaf469fade780
Adam Zmenak <adam@dinteractive.ca>
2013-11-08 19:43:03 -0500

    Empty rails app

---
+++ qc/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qc::Application.config.secret_key_base = 'c5f5e7fa6a88cba7f18ab89bab6449b14d626349f06ee5d07179754b4f007e09c322ace71d5bd10be87d5b3ec1536b6fbb32d5161286f47f5664a46b5b913382'
